64a7acc672ef9d6626fa4e33c4ca8bbfe28b1c31
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-13 16:06:06 +0000

    feat: создать базовую orm модель

--- database.py
+++ database.py
@@ -6,8 +6,28 @@ from sqlalchemy.orm import DeclarativeBase
 from config import settings
 
 
+from sqlalchemy.orm import declared_attr
+from sqlalchemy import Column, Integer, DateTime, func
+
 class Base(DeclarativeBase):  # pylint: disable=too-few-public-methods
-    """Base class for all ORM models."""
+    """Base class for all ORM models. Provides id, created_at, and updated_at columns."""
+
+    @declared_attr.directive
+    def id(cls):
+        return Column(Integer, primary_key=True)
+
+    @declared_attr.directive
+    def created_at(cls):
+        return Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+    @declared_attr.directive
+    def updated_at(cls):
+        return Column(
+            DateTime(timezone=True),
+            default=func.now(),
+            onupdate=func.now(),
+            nullable=False,
+        )
 
 
 engine = create_async_engine(settings.db_url, echo=settings.debug)
