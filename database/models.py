ca0f7533554f243127e98dac75a7df3b6a9e1b74
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-14 16:51:17 +0000

    fix: resolve CI test failures
    Refs: #148
    Correlation-ID: 048f09ca-09ad-41f9-b0f2-a9e209588746

--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, DateTime, func
 class Base:
     id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now(), server_default=func.now())
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now(), default=func.now())
