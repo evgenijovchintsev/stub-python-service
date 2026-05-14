9acabd211a4bb12f488cdea6c789bb136b95d39e
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-14 11:40:49 +0000

    fix: resolve CI test failures
    Refs: #129

--- database.py
+++ database.py
@@ -1,14 +1,1 @@
-"""SQLAlchemy async engine, session factory, and declarative base."""
-
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from database.models.base import Base as BaseModel
-
-from config import settings
-
-
-class Base(BaseModel):  # pylint: disable=too-few-public-methods
-    """Base class for all ORM models."""
-
-
-engine = create_async_engine(settings.db_url, echo=settings.debug)
-async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+from database.models.base import Base
--- database/models/base.py
+++ database/models/base.py
@@ -1,15 +1,3 @@
-from sqlalchemy import Column, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import datetime
 
 Base = declarative_base()
-
-class BaseModel(Base):
-    __abstract__ = True
-    id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
-
-    def __init__(self, **kwargs):
-        super(BaseModel, self).__init__(**kwargs)
-        self.updated_at = datetime.datetime.utcnow()
