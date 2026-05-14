158b5cc50ce9b83952d1f3c0754e16887999f6db
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-14 04:03:17 +0000

    fix: resolve CI test failures

--- database/models.py
+++ database/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, DateTime, func
 
 Base = declarative_base()
 
