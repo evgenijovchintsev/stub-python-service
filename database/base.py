08134158177c823a4a428eeac4201f373c91b4b5
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-14 08:35:57 +0000

    fix: resolve CI test failures

--- database/base.py
+++ database/base.py
@@ -1,9 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Base(Base):
+class BaseModel(Base):
     id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now(), server_default=func.now())
+    created_at = Column(DateTime, server_default=func.now()())
+    updated_at = Column(DateTime, onupdate=func.now()(), server_default=func.now()())
