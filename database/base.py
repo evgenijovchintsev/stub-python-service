0b9a89cabed1bc92b3e82bf66fe32b9b2e701b1d
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-15 07:42:15 +0000

    fix: resolve CI test failures
    Refs: #170
    Correlation-ID: d8b64d52-a3d1-40d7-b124-beee08e47fcf

--- database/base.py
+++ database/base.py
@@ -1,9 +1,3 @@
-from sqlalchemy import Column, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import datetime
-declarative_base()
 
-class Base:
-    id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime, onupdate=datetime.datetime.utcnow)
+Base = declarative_base()
