0a048271034c7f81cf60b8f9119320acb0072345
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-15 08:01:15 +0000

    feat: создать базовую orm модель
    Refs: #172
    Correlation-ID: 6b720fa9-c1bc-43f7-81af-d8c49476b4a7

--- database.py
+++ database.py
@@ -1,14 +1,15 @@
 """SQLAlchemy async engine, session factory, and declarative base."""
 
+from sqlalchemy import Column, Integer, DateTime
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-
-from config import settings
-
+from datetime import datetime
 
 class Base(DeclarativeBase):  # pylint: disable=too-few-public-methods
     """Base class for all ORM models."""
-
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 engine = create_async_engine(settings.db_url, echo=settings.debug)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
