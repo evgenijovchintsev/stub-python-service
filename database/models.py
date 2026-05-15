c87be5cb209f811535d632017f79c0a39b0ae64b
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-15 15:19:50 +0000

    fix: resolve CI test failures
    Refs: #203
    Correlation-ID: 548e30c5-3bff-421d-bc7b-dfc5aee24b28

--- database/models.py
+++ database/models.py
@@ -7,5 +7,5 @@ declare_base = declarative_base()
 class Base(declare_base):
     __abstract__ = True
     id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    created_at = Column(DateTime, default=func.current_timestamp())
+    updated_at = Column(DateTime, default=func.current_timestamp(), onupdate=func.current_timestamp())
