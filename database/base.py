98899c52988dbb198db482598786b601b7f3052a
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-15 17:36:43 +0000

    feat: создать базовую orm модель
    Refs: #207
    Correlation-ID: e511cf0b-8c28-4ba4-9eb6-b022b319f969

--- database/base.py
+++ database/base.py
@@ -1,8 +1,13 @@
 from sqlalchemy import Column, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import datetime
+from datetime import datetime
+
+def get_current_time():
+    return datetime.utcnow()
 
 Base = declarative_base()
 
-def get_utcnow():
-    return datetime.datetime.utcnow()
+class BaseMixin:
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=get_current_time, nullable=False)
+    updated_at = Column(DateTime, default=get_current_time, onupdate=get_current_time, nullable=False)
--- models/user.py
+++ models/user.py
@@ -1,11 +1,6 @@
+from database.base import Base, BaseMixin
 
-def get_utcnow():
-    return datetime.datetime.utcnow()from database.base import Base
-from sqlalchemy import Column, Integer, DateTime, String
-import datetimefrom database.base import Base
-from sqlalchemy import Column, Integer, String, DateTime
-class User(Base):
+class User(BaseMixin, Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime, default=get_utcnow)
-    updated_at = Column(DateTime, onupdate=get_utcnow)
+
+    # Add other columns as needed
