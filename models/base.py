e6a8967ea1cb2c68e7b8a7e28034365bcd5d64e9
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-17 15:27:47 +0000

    fix: resolve CI test failures
    Refs: #215
    Correlation-ID: 5d39f268-91ca-450b-b65c-b95fb5021454

--- models/base.py
+++ models/base.py
@@ -1,11 +1,5 @@
 from sqlalchemy import Column, Integer, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
-
-Base = declarative_base()
-
-class BaseModel(Base):
-    __abstract__ = True
+class Base(object):
     id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, onupdate=datetime.utcnow)
--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
-from .base import BaseModel
+from .base import Base
 from sqlalchemy import Column, String
 
-class User(BaseModel):
+class User(Base):
     __tablename__ = 'users'
     username = Column(String(50), unique=True)
     email = Column(String(120), unique=True)
