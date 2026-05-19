0d49569427fee287659d05a49c9df6a6840be430
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-19 21:45:49 +0000

    feat: создать базовую orm модель
    Refs: #292
    Correlation-ID: 713d5606-350f-49d8-ab8f-5677d49256c9

---
+++ models/sample_model.py
@@ -0,0 +1,8 @@
+from .base import Base
+import datetime
+
+class SampleModel(Base):
+    __tablename__ = 'sample_model'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
