9217c43ac52f47e02b9c3fa2c1899608d96f407d
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-19 10:55:29 +0000

    feat: создать базовую orm модель
    Refs: #276
    Correlation-ID: 9e6b7d22-f784-4d4c-aeb9-d142fcac5b48

---
+++ database/base.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, DateTime, Integer
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+
+declarative_base = declarative_base()
+
+class Base(declarative_base):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, onupdate=datetime.datetime.utcnow)
