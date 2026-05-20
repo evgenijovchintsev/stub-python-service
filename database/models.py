ba0d2733ca8ae7146d8f8d477e774f0971e9746c
Evgeniy Ovchintsev <evgenij.ovchintsev@gmail.com>
2026-05-20 14:19:05 +0000

    fix: resolve CI test failures
    Refs: #304
    Correlation-ID: 18f3fe03-e165-4903-abaa-9f0eff79ebf6

--- database/models.py
+++ database/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+
 import datetime
 
 def get_engine():
